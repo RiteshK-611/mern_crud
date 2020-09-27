cb4c76f7981b3f25d8fee1d3763d7ecb2ea2dc21
Ritesh <riteshkokam@gmail.com>
2020-09-27 15:54:10 +0530

    Second commit

---
+++ client/build/precache-manifest.d2ab0aca24c0143da6c663633e84c65e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7bc2a27310f44d82140b6512150f13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eb80bd91f7db3088270",
+    "url": "/static/css/2.2f73783b.chunk.css"
+  },
+  {
+    "revision": "3eb80bd91f7db3088270",
+    "url": "/static/js/2.e5353f70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e5353f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "856af37929a8fc618e72",
+    "url": "/static/js/main.7437e9f6.chunk.js"
+  },
+  {
+    "revision": "99f0e668b1ba70ac9ad7",
+    "url": "/static/js/runtime-main.a6facfd5.js"
+  }
+]);
